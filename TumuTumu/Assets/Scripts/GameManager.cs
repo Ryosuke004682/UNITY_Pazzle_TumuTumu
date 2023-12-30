afad6c2dd17b8c55a64f87f0b20e4cb4ad36e280
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-12-31 02:58:52 +0900

    一通りできた

--- TumuTumu/Assets/Scripts/GameManager.cs
+++ TumuTumu/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -28,14 +29,27 @@ public class GameManager : MonoBehaviour
 
     private void Start( )
     {
+        audioSource = GetComponent<AudioSource>();
         bubbles = new List<GameObject>();
         panelGameResult.SetActive(false);
 
         SpawnItem(fieldItemCountMax);
     }
 
+
     private void Update( )
     {
+        gameTimer -= Time.deltaTime;
+        textGameTimer.text = "" + (int)gameTimer;
+
+        //ゲーム終了
+        if(0 > gameTimer)
+        {
+            panelGameResult.SetActive(true);
+            enabled = false;
+            return;
+        }
+
         PlayerInput();
     }
 
@@ -57,7 +71,7 @@ public class GameManager : MonoBehaviour
             if(hit2D)
             {
                 GameObject obj = hit2D.collider.gameObject;
-                DeleteItems(obj);
+                CheckItems(obj);
             }
         }
     }
@@ -89,21 +103,34 @@ public class GameManager : MonoBehaviour
         }
     }
 
+
     /// <summary>
     /// 引数と同じ色のアイテムを削除する
     /// </summary>
     /// <param name="target"></param>
-    private void DeleteItems(GameObject target)
+    private void DeleteItems(List<GameObject> checkItems )
     {
-        List<GameObject> checkItems = new List<GameObject>(); 
-
-        checkItems.Add(target);
-
-        //TODO : 全体のアイテムから同じ色を走査する
-
-
         if (checkItems.Count < deleteCount) return;
-        
+
+        //ボーナスとして、オーバーしたカウント*5個消す
+        int overCountBonus = checkItems.Count - deleteCount;
+        overCountBonus *= 5;
+
+        //ランダムなアイテムの削除
+        while (overCountBonus > 0)
+        {
+            int rnd = Random.Range(0 , bubbles.Count);
+            checkItems.Add(bubbles[rnd]);
+            overCountBonus--;
+
+
+            if (overCountBonus == 0)
+            {
+                audioSource.PlayOneShot(seSpecial);
+            }
+        }
+
+
         List<GameObject> destroyItems = new List<GameObject>();
         
         foreach(GameObject item in checkItems)
@@ -123,6 +150,70 @@ public class GameManager : MonoBehaviour
 
         //スコア表示
         textGameScore.text = "" + gameScore;
+
+        audioSource.PlayOneShot(seBubble);
+    }
+
+
+    /// <summary>
+    /// 同じ色のアイテムを返す
+    /// </summary>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    List<GameObject> GetSameItems(GameObject target)
+    {
+        List<GameObject> ret = new List<GameObject>();
+
+        foreach (GameObject item in bubbles)
+        {
+            /*アイテムが無い、同じアイテム、違う色、距離が遠い場合スキップ*/
+
+            Sprite SR_item   = item  .GetComponent<SpriteRenderer>().sprite;
+            Sprite SR_target = target.GetComponent<SpriteRenderer>().sprite;
+
+            //次のアイテムまでの距離を格納
+            float distance = Vector2.Distance(target.transform.position, item.transform.position);
+
+            if (!item || target == item) continue;
+            if (SR_item != SR_target)    continue;
+            if (distance > 1.55f)        continue;
+
+            ret.Add(item);
+        }
+
+        return ret;
+    }
+
+
+    /// <summary>
+    /// 引数と同じアイテムを探す
+    /// </summary>
+    /// <param name="target"></param>
+    private void CheckItems(GameObject target)
+    {
+        List <GameObject> checkItems = new List<GameObject>();
+        checkItems.Add(target);
+
+        int checkIndex = 0; //チェック済みのインデクス数
+
+        while(checkIndex < checkItems.Count)
+        {
+            List<GameObject> sameItems = GetSameItems(checkItems[checkIndex]);
+
+            checkIndex++;
+
+            foreach(GameObject item in sameItems)
+            {
+                if (checkItems.Contains(item)) continue;
+                checkItems.Add(item);
+            }
+        }
+        DeleteItems(checkItems);
+    }
+
+    public void OnClickRetry()
+    {
+        SceneManager.LoadScene("TumuTumuScene");
 
     }
 
